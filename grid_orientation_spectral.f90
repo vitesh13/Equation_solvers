701ea806366451f8996811be53c1ca11e0cda505
Vitesh Shah <vitesh.manchester@gmail.com>
2020-03-19 18:18:06 +0100

    Extra comments

--- grid_orientation_spectral.f90
+++ grid_orientation_spectral.f90
@@ -1,3 +1,7 @@
+!< Currently the code is being written with the following equation in mind
+!< \tau \frac{\partial \theta}{\partial t} = \nabla \cdot \left( 2 h(\eta) \nabla \theta \right)
+!< This is the first part of the equation 40 mentioned in https://doi.org/10.1080/14786435.2012.713135
+
 module grid_orientation_spectral
 
 !public :: &
